893ddd41d369e4413d38e1b371767fae29a35b1c
kiranshahi <its.kiran.shahi@gmail.com>
2017-04-21 09:49:43 +0545

    fine amount done.

--- Library Management System AD/Admin/ReturnLoan.aspx.cs
+++ Library Management System AD/Admin/ReturnLoan.aspx.cs
@@ -21,7 +21,7 @@ namespace Library_Management_System_AD.Admin
 
                     string connectString =
                         WebConfigurationManager.ConnectionStrings["dbConnectionString"].ConnectionString;
-                    string queryString = "SELECT loan_types.type, book_copies.copy_number, members.name, loans.issued_date, loans.returned_date FROM loans JOIN loan_types ON loan_types.id = loans.loan_type_id JOIN book_copies ON book_copies.id =loans.book_copy_id JOIN members ON members.id =loans.member_id where loans.id =' " + loanId + " ' ";
+                    string queryString = "SELECT loan_types.type, loan_types.max_duration, book_copies.copy_number, members.name, loans.issued_date, loans.returned_date, membership_types.penalty_charge FROM loans JOIN loan_types ON loan_types.id = loans.loan_type_id JOIN book_copies ON book_copies.id =loans.book_copy_id JOIN members ON members.id =loans.member_id JOIN membership_types on members.membership_type_id = membership_types.id where loans.id =' " + loanId + " ' ";
 
                     SqlConnection myConnection = new SqlConnection(connectString);
                     myConnection.Open();
@@ -35,10 +35,31 @@ namespace Library_Management_System_AD.Admin
                         bookCopy.Text = (myReader["copy_number"].ToString());
                         member.Text = (myReader["name"].ToString());
                         txtIssuedDate.Text = String.Format("{0:yyyy-MM-dd }", myReader["issued_date"]);
+                        DateTime issueDate = Convert.ToDateTime(String.Format("{0:yyyy-MM-dd }", myReader["issued_date"]));
+                        Int32 maxDuration = Convert.ToInt32(myReader["max_duration"].ToString());
+                        DateTime dueDate = issueDate.AddDays(maxDuration);
+                        txtDueDate.Text = String.Format("{0:yyyy-MM-dd }", dueDate);
+                        Int32 fineAmount = 0;
+                        Int32 fineRate = Convert.ToInt32(myReader["penalty_charge"].ToString());
+                        DateTime today = Convert.ToDateTime(DateTime.Now.ToString("yyyy-MM-dd"));
                         if (!String.IsNullOrWhiteSpace(myReader["returned_date"].ToString()))
                         {
+                            DateTime returnedDate = Convert.ToDateTime(String.Format("{0:yyyy-MM-dd }", myReader["returned_date"]));
                             txtReturnedDate.Text = String.Format("{0:yyyy-MM-dd }", myReader["returned_date"]);
                             txtReturnedDate.Attributes.Add("readonly", "readonly");
+                            if (dueDate < returnedDate)
+                            {
+                                TimeSpan exceedTimeSpan = returnedDate - dueDate;
+                                Int32 exceedDays = Convert.ToInt32(exceedTimeSpan.TotalDays);
+                                fineAmount = exceedDays * fineRate;
+                                txtFineAmount.Text = "Rs. "+fineAmount.ToString();
+                            }
+                        } else if (today > dueDate)
+                        {
+                            TimeSpan exceedTimeSpan = today - dueDate;
+                            Int32 exceedDays = Convert.ToInt32(exceedTimeSpan.TotalDays);
+                            fineAmount = exceedDays * fineRate;
+                            txtFineAmount.Text = "Rs. " + fineAmount.ToString();
                         }
                     }
                     myReader.Close();
--- Library Management System AD/Admin/ReturnLoan.aspx.designer.cs
+++ Library Management System AD/Admin/ReturnLoan.aspx.designer.cs
@@ -94,6 +94,15 @@ namespace Library_Management_System_AD.Admin {
         protected global::System.Web.UI.WebControls.TextBox txtDueDate;
         
         /// <summary>
+        /// txtFineAmount control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtFineAmount;
+        
+        /// <summary>
         /// txtReturnedDate control.
         /// </summary>
         /// <remarks>
